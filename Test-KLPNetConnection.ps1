433527693d7cb36af57a1d96d9891e2aeb7bc6af
tombs4 <thomas.s.r.bolton@gmail.com>
2021-08-02 15:50:26 -0400

    Added comments, changed to IF Index

--- Test-KLPNetConnection.ps1
+++ Test-KLPNetConnection.ps1
@@ -1,4 +1,5 @@
 param (
+    # the servers or IPs to by tested
     [Parameter(Mandatory=$true,Position=0)]
     [string[]]
     $servers,
@@ -9,10 +10,14 @@ param (
 )
 
 
-
+# iterates throught the provided list of servers
 foreach ($endpoint in $servers) {
+    # on each iteration of the server list, iterates through the port list
     foreach ($port in $portlist) {
+        # tests each port in the sequence provided
         Test-NetConnection -ComputerName $endpoint -Port $port | 
+            # adds in the timestamp property to a new custom object created by the select-object commandlet. Adds in
+            # the timestamp at the time the custom object is created.
             Select-Object @{
                     Label="Timestamp"
                     Expression={get-date -format yyyy-MM-ddTHH:mm:ss}
@@ -23,7 +28,7 @@ foreach ($endpoint in $servers) {
                     Expression={($_.RemoteAddress.IPAddressToString)}
                 },
                 RemotePort,
-                InterfaceAlias,
+                InterfaceIndex,
                 TcpTestSucceeded
     }
 }
